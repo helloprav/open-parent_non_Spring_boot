6994fe2d29c0fd327f1f7db7d4cd9751d0256b0e
praku <praku@LAPTOP-22K85V8G>
2018-12-29 22:55:07 +0530

    deleted user-security proj from parent project

--- open-commons-userandsecurity/src/main/java/org/school/userandsecurity/service/repository/FunctionRepository.java
+++
@@ -1,8 +0,0 @@
-package org.school.userandsecurity.service.repository;
-
-import org.school.userandsecurity.service.entity.Function;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FunctionRepository extends JpaRepository<Function, Long> {
-
-}
---
+++ open-gui/WebContent/npm_install.bat
@@ -0,0 +1,7 @@
+npm i --save angular
+npm i --save angular-ui-router
+npm i --save angular-ui-bootstrap
+npm i --save jquery
+npm i --save @fortawesome/fontawesome-free
+npm i --save bootstrap
+npm i --save popper.js@^1.14.3
